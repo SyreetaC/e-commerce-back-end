2a5a2851a36340e10ab319b4bf1b7d64e1f815da
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-06-01 10:00:18 +0100

    delete route added for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,7 +16,7 @@ router.get("/", async (req, res) => {
     });
     res.status(200).json(categories);
   } catch (error) {
-    console.error(error);
+    console.error(error.message);
     res.status(500).json({ error: "Failed to get categories" });
   }
   // find all categories
@@ -35,7 +35,7 @@ router.get("/:id", async (req, res) => {
       res.status(404).json({ [ERROR]: "No category found with this id" });
     }
   } catch (error) {
-    console.error(error);
+    console.error(error.message);
     res.status(500).json({ error: "Failed to get category with this id" });
   }
   // find one category by its `id` value
@@ -50,7 +50,7 @@ router.post("/", async (req, res) => {
     res.status(200).json(newCategory);
     //what object is being created?
   } catch (error) {
-    console.error(error);
+    console.error(error.message);
     res.status(500).json({ error: "Failed to create new category" });
   }
   // create a new category
@@ -70,15 +70,28 @@ router.put("/:id", async (req, res) => {
       res.status(404).json({ [ERROR]: "No category found with this id" });
     }
   } catch (error) {
-    console.error(error);
+    console.error(error.message);
     res.status(500).json({ error: "Failed to get category with this id" });
   }
 
   // update a category by its `id` value
 });
 
-router.delete("/:id", (req, res) => {
-  // delete a category by its `id` value
+//ALSO NEEDS FIXING
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedCategory = await Category.destroy({ where: { id } });
+    res.status(200).json(deletedCategory);
+    if (!deletedCategory) {
+      res.status(404).json({ [ERROR]: "No category found with this id" });
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Failed to delete category" });
+  }
 });
+
+// delete a category by its `id` value
 
 module.exports = router;
