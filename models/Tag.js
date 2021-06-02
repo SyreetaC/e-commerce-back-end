3c98d840e57cd22d91cca5befd333524b1b8f0ad
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-06-02 20:55:55 +0100

    fixed foreign keys

--- models/Tag.js
+++ models/Tag.js
@@ -12,7 +12,7 @@ Tag.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    tag_name: { type: DataTypes.STRING },
+    tag_name: { type: DataTypes.STRING, allowNull: true },
   },
   {
     sequelize,
