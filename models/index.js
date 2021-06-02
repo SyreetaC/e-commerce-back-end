8809f982d08bf446f9eb29255b6724b19cf6926c
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-06-02 21:54:09 +0100

    wip

--- models/index.js
+++ models/index.js
@@ -8,14 +8,11 @@ const ProductTag = require("./ProductTag");
 Product.belongsTo(Category, {
   foreignKey: "category_id",
   onDelete: "CASCADE",
-  onUpdate: "CASCADE",
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: "category_id",
-  onDelete: "CASCADE",
-  onUpdate: "CASCADE",
 });
 
 // Products belongToMany Tags (through ProductTag)
